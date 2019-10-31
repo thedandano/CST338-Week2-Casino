8ab5fab7c0ab5e1893ebda1a7d1963ebc082908d
thedandano <dandano2006@gmail.com>
2019-10-31 12:09:47 -0700

    Update Assig2.java
    Added getBet() method

--- Casino/src/Assig2.java
+++ Casino/src/Assig2.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 
-public class Main
+public class Assig2
 {
    public static void main(String []args)
    {
-      ThreeString test = new ThreeString();
+      /*ThreeString test = new ThreeString();
       System.out.println("this is a test get should be blank: " + test.getString1());
       System.out.println("set to null: " + test.setString1(null));
       System.out.println("this should say be blank: " + test.getString1());
@@ -13,6 +14,52 @@ public class Main
       
       test.saveWinnings(5);
       System.out.println(test.displayWinnings());
+      */
+
+      getBet();
+   }
+   
+   /**
+    * Prompts the user for their bet and validates the bet. 
+    * @return The users Bet.
+    */
+   static int getBet()
+   {
+      Scanner in = new Scanner(System.in);
+      int userBet;
+      boolean invalidBet = false;
+      
+      do 
+      {
+         System.out.print("Place your bet! Must be 1-100 or enter 0 to exit: ");
+         userBet = in.nextInt();
+         System.out.println("You bet: " + userBet);
+         
+         //validates user input and provides appropriate responses
+         if(userBet < 0)
+         {
+            System.out.println("Invalid Bet! Try again...\n");
+            invalidBet = true;
+         }
+         else if(userBet > 100)
+         {
+            System.out.println("Invalid Bet! Try again...\n");
+            invalidBet = true;
+         }
+         else if(userBet == 0)
+         {
+            System.out.println("Thanks for playing!");
+            System.exit(0);
+         }
+         else
+         {
+            System.out.println("Great!");
+            invalidBet = false;
+         }
+         
+      }while(invalidBet);
+      
+      return userBet; 
    }
 }
 
@@ -20,7 +67,7 @@ class ThreeString
 {
    public static final int MAX_LENS = 20;
    public static final int MAX_PULLS = 40;
-   private static int numPull = 0;
+   private static int numPulls = 0;
    //array to track winnings
    public static int[] pullWinnings = new int[MAX_PULLS];
    //Instance Variables
@@ -137,7 +184,7 @@ class ThreeString
    
    public boolean saveWinnings(int winnings)
    {
-      pullWinnings[0] = winnings;
+      pullWinnings[numPulls] = winnings;
       return false;
    }
    /**
@@ -151,11 +198,10 @@ class ThreeString
       
       for(int x = 0; x < pullWinnings.length; x++) 
       {
-         winnings += pullWinnings[x];
-         sum =+ pullWinnings[x];
+         sum += pullWinnings[x];
+         winnings += pullWinnings[x];        
       }
-        
-      
-      return winnings + " Total Winnings: " + sum;
+
+      return winnings + " Total Winnings: $" + sum;
    }
 }
