bf930d0cff27f2a26352a902305bdb1e38571b2e
thedandano <dandano2006@gmail.com>
2019-10-30 22:07:09 -0700

    Constructor and Methods
    Added a constructor
    Added a String validator
    Added Getters and Setters
    Tested Setters and Getters and String Validator

--- Casino/src/Main.java
+++ Casino/src/Main.java
@@ -3,7 +3,12 @@ public class Main
 {
    public static void main(String []args)
    {
-      
+      ThreeString test = new ThreeString();
+      System.out.println("this is a test get should be blank: " + test.getString1());
+      System.out.println("set to null: " + test.setString1(null));
+      System.out.println("this should say be blank: " + test.getString1());
+      System.out.println("set to hola: " + test.setString1("hola"));
+      System.out.println("this should say hola: " + test.getString1());
    }
 }
 
@@ -11,9 +16,108 @@ class ThreeString
 {
    public static final int MAX_LENS = 20;
    public static final int MAX_PULLS = 40;
+   private static int numPull = 0;
    //array to track winnings
    public static int[] pullWinnings = new int[MAX_PULLS];
    //Instance Variables
    private String string1, string2, string3;
+   
+   /**
+    * Constructor initializes instance variables
+    */
+   ThreeString()
+   {
+      string1 = "";
+      string2 = "";
+      string3 = "";
+   }
+   
+   /**
+    * Helper method returns true if the string is not null and if the string
+    * length is less than or equal to the MAX_LENS which is 20 chars.
+    * @param String
+    * @return true if the String is not null and <= MAX_LENS (20 chars)
+    */
+   private boolean validString(String str)
+   {
+      if(str != null && str.length() <= MAX_LENS)
+         return true;
+      
+      return false;
+   }
+   
+   /**
+    * Accessor for string1
+    * @return string1
+    */
+   public String getString1()
+   {
+      return string1;
+   }
+   /**
+    * Accessor for string2
+    * @return string2
+    */
+   public String getString2()
+   {
+      return string2;
+   }
+   /**
+    * Accessor for string3
+    * @return string3
+    */
+   public String getString3()
+   {
+      return string3;
+   }
+   
+   /**
+    * Mutator for string1
+    * @param a string parameter
+    * @return true if the string passed is valid
+    * @return false if the string passed is invalid
+    */
+   public Boolean setString1(String s1)
+   {
+      if(validString(s1) == true)
+      {
+         string1 = s1;
+         return true;
+      }
+      
+      return false; 
+   }
+   /**
+    * Mutator for string2
+    * @param a string parameter
+    * @return true if the string passed is valid
+    * @return false if the string passed is invalid
+    */
+   public Boolean setString2(String s2)
+   {
+      if(validString(s2) == true)
+      {
+         string2 = s2;
+         return true;
+      }
+      
+      return false; 
+   }
+   /**
+    * Mutator for string3
+    * @param a string parameter
+    * @return true if the string passed is valid
+    * @return false if the string passed is invalid
+    */
+   public Boolean setString3(String s3)
+   {
+      if(validString(s3) == true)
+      {
+         string3 = s3;
+         return true;
+      }
+      
+      return false; 
+   }
    
 }
