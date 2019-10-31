1f5c2ce085f1873445c35f4dde622ff2de5b93e1
thedandano <dandano2006@gmail.com>
2019-10-30 21:21:08 -0700

    First Commit

---
+++ Casino/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main
+{
+   public static void main(String []args)
+   {
+      
+   }
+}
+
+class ThreeString
+{
+   public static final int MAX_LENS = 20;
+   public static final int MAX_PULLS = 40;
+   //array to track winnings
+   public static int[] pullWinnings = new int[MAX_PULLS];
+   //Instance Variables
+   private String string1, string2, string3;
+   
+}
